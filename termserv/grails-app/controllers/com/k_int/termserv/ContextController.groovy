f177397a9100dd36079c325e854ce53cdc9e6fe2
ianibo <ianibbo@googlemail.com>
2012-02-07 15:21:18 +0000

    updated

--- termserv/grails-app/controllers/com/k_int/termserv/ContextController.groovy
+++ termserv/grails-app/controllers/com/k_int/termserv/ContextController.groovy
@@ -2,5 +2,16 @@ package com.k_int.termserv
 
 class ContextController {
 
-    def index() { }
+  def index() { 
+  }
+
+  def lookup() {
+    log.debug("Lookup term ${params.q}");
+    def result = [:]
+
+    // Try to find a term where candidate_identifier == params.q
+
+    // If params.add=true and the term is not found, check for it's presence in the candidate term list and add it if not found.
+    result;
+  }
 }
