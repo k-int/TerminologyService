f7423e5aa2ca9af5b64925428472f892603541d3
ianibo <ianibbo@googlemail.com>
2012-02-07 15:32:53 +0000

    Added mongo actions to controller

--- termserv/grails-app/controllers/com/k_int/termserv/ContextController.groovy
+++ termserv/grails-app/controllers/com/k_int/termserv/ContextController.groovy
@@ -1,13 +1,22 @@
 package com.k_int.termserv
 
+import com.gmongo.GMongo
+
 class ContextController {
 
   def index() { 
   }
 
   def lookup() {
+    def mongo = new com.gmongo.GMongo();
+    def termserv_db = mongo.getDB("ki_termserv")
+
     log.debug("Lookup term ${params.q}");
     def result = [:]
+
+    def located_term = termserv_db.terms.findOne(identifier: params.q)
+
+    // def mongo_store_result = termserv_db.terms.save(term_as_pojo)
 
     // Try to find a term where candidate_identifier == params.q
 
